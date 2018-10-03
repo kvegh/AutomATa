1e8a789c089a4776e296c9665946eb6d803b78db
Karoly VEGH <kvegh@redhat.com>
2018-10-03 16:20:46 -0400

    dynamic inventory with python-json + win host renaming works

---
+++ dynamic_custom_inventory.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ssh ansible@gateway "sudo virsh list --state-running --name" | grep -v ansible | grep -v ^$ > inv_input
+
+cat <<__EOF__ > inventory
+[barelaptop]
+gateway
+
+[win_vms]
+__EOF__
+
+for host in `grep win inv_input` ; do echo ${host} ansible_host=`ssh ansible@gateway "sudo virsh domifaddr $host" | grep ipv4 | awk '{print $4}' | cut -d/ -f1` ; done >> inventory
+echo -e "\n[rhel_vms]" >> inventory 
+grep rhel inv_input >> inventory 
+
+if [ "$1" == "--list" ] ; then
+
+python inventory2json.py inventory
+
+elif [ "$1" == "--host" ]; then
+  echo '{"_meta": {"hostvars": {}}}'
+else
+  echo "{ }"
+fi
