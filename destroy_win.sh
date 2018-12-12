cfadd730fb4ef2ba8b9aa092882067c9e8a0e909
Karoly VEGH <kvegh@redhat.com>
2018-12-12 09:37:11 -0500

    fixed deployment issue

--- destroy_win.sh
+++ destroy_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -b -i inventory/dynamic_custom_inventory.sh -e number_of_hosts=2 -e project_name=${1} win_vmdestroy.yml 
+ansible-playbook -l gateway -b -i inventory/dynamic_custom_inventory.sh -e number_of_hosts=2 -e project_name=${1} win_vmdestroy.yml 
--- prep_win.sh
+++ prep_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ansible-playbook -l gateway -b -i inventory/dynamic_custom_inventory.sh -e number_of_hosts=2 -e project_name=${1} win_vmcreate.yml 
-ansible-playbook -l gateway -i inventory/dynamic_custom_inventory.sh win_hostname.yml
+ansible-playbook -i inventory/dynamic_custom_inventory.sh win_hostname.yml
