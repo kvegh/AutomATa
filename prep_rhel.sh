348eca4d8c223fa8f7f12d8802133858e0c80ed3
Karoly VEGH <kvegh@redhat.com>
2018-12-12 05:55:49 -0500

    limiting these to barelaptop

--- prep_rhel.sh
+++ prep_rhel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ansible-playbook -b -i inventory/dynamic_custom_inventory.sh -e number_of_hosts=2 -e project_name=${1} rhel_vmcreate.yml 
-ansible-playbook -i inventory/dynamic_custom_inventory.sh -e number_of_hosts=2 -e project_name=${1} rhel_sshkeysremove.yml 
-ansible-playbook -i inventory/dynamic_custom_inventory.sh -e number_of_hosts=2 -e project_name=${1} rhel_sshkeyssetup.yml 
+ansible-playbook -l gateway -b -i inventory/dynamic_custom_inventory.sh -e number_of_hosts=2 -e project_name=${1} rhel_vmcreate.yml 
+ansible-playbook -l gateway -i inventory/dynamic_custom_inventory.sh -e number_of_hosts=2 -e project_name=${1} rhel_sshkeysremove.yml 
+ansible-playbook -l gateway -i inventory/dynamic_custom_inventory.sh -e number_of_hosts=2 -e project_name=${1} rhel_sshkeyssetup.yml 
