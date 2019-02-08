a2f52c22029cbabbbbe8eb5e2d7b87b5d9b9a1fb
Karoly VEGH <kvegh@redhat.com>
2019-02-08 11:59:01 -0500

    fixed username. How did this ever work?

--- inventory_hetzner/dynamic_custom_inventory.sh
+++ inventory_hetzner/dynamic_custom_inventory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh hetzner "sudo virsh list --name --all" | grep -v ansible | grep -v image |  grep -v ^$ > inv_input
+ssh ansible@hetzner "sudo virsh list --name --all" | grep -v ansible | grep -v image |  grep -v ^$ > inv_input
 
 cat <<__EOF__ > inventory_hetzner/hosts
 [baremetal]
@@ -9,7 +9,7 @@ hetzner
 [win_vms]
 __EOF__
 
-for host in `grep win inv_input` ; do echo ${host} ansible_host=`ssh ansible@gateway "sudo virsh domifaddr $host" | grep 192.168.122 | awk '{print $4}' | cut -d/ -f1` ; done >> inventory_hetzner/hosts
+for host in `grep win inv_input` ; do echo ${host} ansible_host=`ssh ansible@hetzner "sudo virsh domifaddr $host" | grep 192.168.122 | awk '{print $4}' | cut -d/ -f1` ; done >> inventory_hetzner/hosts
 echo -e "\n[rhel_vms]" >> inventory_hetzner/hosts
 grep rhel inv_input >> inventory_hetzner/hosts
 
