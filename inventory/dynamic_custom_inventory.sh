53fa443ad11f45919206f4e22f46d34a97a58838
Karoly VEGH <kvegh@redhat.com>
2019-07-30 14:56:33 -0400

    working around having to have templates in custom credentials

--- inventory/dynamic_custom_inventory.sh
+++ inventory/dynamic_custom_inventory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh ansible@gateway "sudo virsh list --name --all" | grep -v ansible | grep -v win2k16r2 | grep -v image | grep -v ^$ > inv_input
+ssh -i /opt/data/skeys/`whoami`/cred1 ansible@gateway "sudo virsh list --name --all" | grep -v ansible | grep -v win2k16r2 | grep -v image | grep -v ^$ > inv_input
 
 cat <<__EOF__ > inventory/hosts
 [baremetal]
