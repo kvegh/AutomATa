852155ab884c6bdc48707313d4bc2146ea88402f
Karoly VEGH <kvegh@redhat.com>
2019-07-30 15:24:56 -0400

    this workaround is getting uglier by the hour. Go with custom credentials and templates, folks

--- inventory/dynamic_custom_inventory.sh
+++ inventory/dynamic_custom_inventory.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-mkdir ~/.ssh 
-chmod 700 ~/.ssh
-ssh-keyscan gateway > ~/.ssh/known_hosts
-ssh -i /opt/data/skeys/`whoami`/cred1 ansible@gateway "sudo virsh list --name --all" | grep -v ansible | grep -v win2k16r2 | grep -v image | grep -v ^$ > inv_input
+if [ "`whoami`" == "awx" ] 
+then 
+  mkdir ~/.ssh 
+  chmod 700 ~/.ssh
+  ssh-keyscan gateway > ~/.ssh/known_hosts
+fi
+  ssh -i /opt/data/skeys/`whoami`/cred1 ansible@gateway "sudo virsh list --name --all" | grep -v ansible | grep -v win2k16r2 | grep -v image | grep -v ^$ > inv_input
 
 cat <<__EOF__ > inventory/hosts
 [baremetal]
