9706895b875db789ea2f7bb16246e64cef3eeba3
Karoly VEGH <kvegh@redhat.com>
2020-11-16 17:01:48 -0500

    The insights demo elements

---
+++ insights_destroy.sh
@@ -0,0 +1,3 @@
+sudo virsh destroy rhel-kvegh-$1-711 ; sudo virsh undefine rhel-kvegh-$1-711 --remove-all-storage
+sudo virsh destroy rhel-kvegh-$1-741 ; sudo virsh undefine rhel-kvegh-$1-741 --remove-all-storage
+sudo virsh destroy rhel-kvegh-$1-801 ; sudo virsh undefine rhel-kvegh-$1-801 --remove-all-storage
