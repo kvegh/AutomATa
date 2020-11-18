ab7871f81c4d471567e24fd2cf7e2f50a68a0a91
Karoly VEGH <kvegh@redhat.com>
2020-11-18 04:23:54 -0500

    additional insights demo playbooks, updated rhel_webserver for RHEL 8/python3, updated redhat.insights role

--- create_insights.sh
+++ create_insights.sh
@@ -1,3 +1,4 @@
+cp /home/ansible/.ssh/known_hosts.bak /home/ansible/.ssh/known_hosts
 ./prep_insights.sh 1 kvegh-$1-71 7.1 
 ./prep_insights.sh 1 kvegh-$1-74 7.4 
 ./prep8_insights.sh 1 kvegh-$1-80 8.0
