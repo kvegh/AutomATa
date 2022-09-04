f21355099e41e0e06f290f49eb0f13001858e142
Karoly VEGH <karoly.vegh@gmail.com>
2022-09-04 17:34:44 +0200

    cleaning up further for cli deployments too

--- destroy_rhel.sh
+++ destroy_rhel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-playbook -i inventory/dynamic_custom_inventory.sh -e number_of_hosts=2 -e project_name=${1} rhel_sshkeysremove.yml 
+ansible-playbook -i inventory/dynamic_custom_inventory.sh -e number_of_hosts=${2} -e project_name=${1} rhel_sshkeysremove.yml 
 ansible-playbook -l gateway -b -i inventory/dynamic_custom_inventory.sh -e number_of_hosts=${2} -e project_name=${1} rhel_vmdestroy.yml 
